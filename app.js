127a52b2fe30362588df8ebd3864c95d5130c4d9
AmauriHopewell <amauri.hopewell@snhu.edu>
2026-07-12 03:17:18 -0400

    Partially migrate to MVC template
    Adds route, controller, and handlebar files to replace index and travel page. Remaining pages still route to old public .html files.

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+var path = require('path');
+var hbs = require('hbs');   // if not already present
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+//app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
+//adding for module 2
+var travelRouter = require('./app_server/routes/travel');
+app.use('/travel', travelRouter);
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views', 'partials'));
 
 app.use(logger('dev'));
 app.use(express.json());
